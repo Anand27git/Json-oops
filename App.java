215fb9e24e7da1024d0db8f6cc4a7bf8b354010e
Anand27git <anandkumar256k@gmail.com>
2021-06-06 18:18:04 +0530

    added Inventory Manager

--- App.java
+++ App.java
@@ -1,22 +1,32 @@
 package com.Bridgelabz.jsonPrograms;
 
 import java.util.List;
-import com.Bridgelabz.jsonPrograms.model.Stock;
+import com.Bridgelabz.jsonPrograms.model.*;
 
-/**********
+
+
+/********************
  * 
- * @author ANAND 
- * purpose :Json Inventory Data Management Using Rice,pulses,wheat
- * Problem Statement 1: prints the total items in the inventory
+ * @author ANAND
+ *purpose: added Inventory Manager
  *
- *
- *********/
-public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		JsonUtility jsonutil = new JsonUtility();
-		List<Stock> listStock = jsonutil.convertJsonToObj(
-				"F:\\Java\\jsonPrograms\\src\\main\\java\\com\\Bridgelabz\\jsonPrograms\\model\\inventory.json");
-		System.out.println("Total items in the Inventory = " + listStock.size());
-	}
+ ***********************/
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        JsonUtility jsonutil =new JsonUtility();
+        List<Stock> listStock = jsonutil.convertJsonToObj("F:\\Java\\jsonPrograms\\src\\main\\java\\com\\Bridgelabz\\jsonPrograms\\model\\inventory.json");     
+        System.out.println("Total items in the Inventory = " + listStock.size());   
+        InventoryManager manager = new InventoryManager();
+        manager.readInventory(listStock);
+        manager.getTotalRiceWeight();
+        manager.getTotalWheatWeight();
+        manager.getTotalPulseWeight();
+        manager.getPriceOfRicePerKg();
+        manager.getPriceOfWheatPerKg();
+        manager.getPriceOfPulsesPerKg();
+        System.out.println("Total Inventory Cost = " +(manager.getTotalValueOfRice() + manager.getTotalValueOfWheat() + manager.getTotalValueOfPulses()));
+    }
 }
